511e85b50aa15c16522cdd9e1c77176e902839a2
LucasDeSousaR <rodrigueslucas1114@gmail.com>
2023-11-27 15:34:27 -0300

    Mudança no layout da pagina

--- ControleVeiculo/App_Start/RouteConfig.cs
+++ ControleVeiculo/App_Start/RouteConfig.cs
@@ -14,10 +14,10 @@ namespace ControleVeiculo
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Veiculos", action = "Index", id = UrlParameter.Optional }
-            );
+                    name: "Default",
+                    url: "{controller}/{action}/{id}",
+                    defaults: new { controller = "Veiculos", action = "Index", id = UrlParameter.Optional }
+                );
             routes.MapRoute(
                 name: "Search",
                 url: "Veiculos/Search/{searchString}",
